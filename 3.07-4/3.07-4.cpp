d36805376686b4541622136378446c83a568b39c
ifiamawood <821371350@qq.com>
2020-03-27 14:43:09 +0800

    zhege

--- 3.07-4/3.07-4.cpp
+++ 3.07-4/3.07-4.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include<opencv.hpp>
+#include<iostream>
+
+using namespace cv;
+using namespace std;
 
 
 int main()
 {
+	VideoCapture cap;
+	cap.open(0);
+	cv::Mat  output1,output2, input,output11,output22;
+	cap >> input;
+	cv::Sobel(input, output1,CV_16SC1 , 1, 0, 3, 1, 0, BORDER_DEFAULT);
+	cv::Sobel(input, output2, CV_16SC1, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+	convertScaleAbs(output1,output11);
+	convertScaleAbs(output2, output22);
+	imshow("xÒ»½×", output11);
+	waitKey(0);
+	imshow("yÒ»½×", output22);
+	waitKey(0);
     return 0;
 }
 
